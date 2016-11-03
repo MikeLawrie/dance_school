a626a0d230df5d42d5a91e44137b85a6d4dce197
balashovmb <balashovmb@gmail.com>
2016-11-03 21:16:25 +0500

    User lessons index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  root 'lessons#index'
 
-    resources :lessons, only: [:index] 
+    resources :lessons, only: [:index, :show] 
 
 
   namespace :admin do
     resources :lessons
+
+  get 'welcome/index'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
