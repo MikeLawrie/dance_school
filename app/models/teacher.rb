6cd57b56674c7d7c0657ef2b88983eb58df783e8
balashovmb <balashovmb@gmail.com>
2016-12-01 16:23:13 +0500

    Added photo validations, change size

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,8 +2,9 @@ class Teacher < ApplicationRecord
   has_many :lessons_teachers
   has_many :lessons, through: :lessons_teachers
   
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/photos/:style/missing.png"
-  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+  has_attached_file :photo, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/photos/:style/missing.png"
+  validates_attachment_content_type :photo, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  validates_attachment_size :photo, :less_than => 3.megabytes
 
   scope :by_name, -> { order(:name) }
 
