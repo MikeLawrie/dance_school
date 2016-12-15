030fd04858f3b97dc25000532f116c7a09db3888
balashovmb <balashovmb@gmail.com>
2016-12-15 15:06:42 +0500

    Teacher photo validations fix

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,8 +3,9 @@ class Teacher < ApplicationRecord
   has_many :lessons, through: :lessons_teachers
   
   has_attached_file :photo, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/photos/:style/missing.png"
-  validates_attachment_content_type :photo, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-  validates_attachment_size :photo, :less_than => 3.megabytes
+  validates_attachment :photo,
+    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"]},
+    size: { in: 0..2.megabytes }
 
   scope :by_name, -> { order(:name) }
 
