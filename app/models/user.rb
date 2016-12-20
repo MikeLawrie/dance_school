e1599e7aca2f08d87a63715376970939d0fb63a0
balashovmb <balashovmb@gmail.com>
2016-12-20 11:24:41 +0500

    Added student_to_user callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
 
 #  after_create :create_student
 
-  after_save :create_student, :if => proc { |l| l.confirmed_at_changed? && l.confirmed_at_was.nil? }
+  after_save :student_to_user, :if => proc { |l| l.confirmed_at_changed? && l.confirmed_at_was.nil? }
+  
   private
 
   def create_student
@@ -18,6 +19,14 @@ class User < ApplicationRecord
     student.first_name = self.first_name
     student.last_name = self.last_name
     student.save
+  end
+
+  def student_to_user
+    if student = Student.where(email: self.email).first
+      self.student = student
+    else 
+      create_student
+    end
   end
 
 end
